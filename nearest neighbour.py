dd519094cebf026c52b70e680cd11fd527934747
Oxavia <61558652+1447-Oxavia@users.noreply.github.com>
2022-11-02 16:14:42 +0100

    nn presentation program

--- nearest neighbour.py
+++ nearest neighbour.py
@@ -1,20 +1,38 @@
+from __future__ import annotations
 import random
+import itertools as it
+from turtle import color, width
+import matplotlib.pyplot as plt
+from Graph import Graph
+from Knoten import Knoten
 
 class Knoten(object):
-    def __init__(self, name, gewicht):
+    def __init__(self, name, xKoordinate, yKoordinate, besucht):
         self.name = name
-        self.gewicht = gewicht 
+        self.x = xKoordinate
+        self.y = yKoordinate
+        self.besucht = besucht
         self.vorgaenger = []
+
+    def getX(self):
+        return self.x
+
+    def setX(self, neuesX:int):
+        self.x = neuesX
+        return self.x
+
+    def getY(self):
+        return self.y
+    
+    def setY(self, neuesY:int):
+        self.y = neuesY
+        return self.y
 
     def addVorgaenger(self, vorgaengerName:int):
         self.vorgaenger.append(vorgaengerName)
         
     def getVorgaenger(self):
         return self.vorgaenger
-
-    def incGewicht(self, inc:int):
-        if self.gewicht == "u":
-            self.gewicht = inc
     
     def setName(self, randomName:int):
         self.name = randomName
@@ -22,9 +40,12 @@ class Knoten(object):
 
     def getName(self):
         return self.name
-            
+
+    def getDistance(self, startKnoten, zielKnoten):
+        return ((startKnoten.x - zielKnoten.x) ** 2 + (startKnoten.y - zielKnoten.y) ** 2) ** 0.5
+        
 class Graph(object):
-    def __init__(self, ):
+    def __init__(self):
         self.KnotenMenge = []
         self.KantenMenge = []
 
@@ -37,9 +58,9 @@ class Graph(object):
                 return True
             else:
                 return False
-
-    def addKnoten(self, nameKnoten:int, gewichtKnoten:int):
-        k = Knoten(nameKnoten,gewichtKnoten)
+                
+    def addKnoten(self, nameKnoten:int, xKoordinate:int, yKoordinate:int, besucht:bool):
+        k = Knoten(nameKnoten, xKoordinate, yKoordinate, besucht)
         self.KnotenMenge.append(k)
        
 
@@ -63,10 +84,12 @@ class Graph(object):
             else:
                 return False
 
-    def addKante(self, nameStartKnoten:int, nameZielKnoten:int, laenge:int):
-        self.KantenMenge.append([nameStartKnoten , nameZielKnoten, laenge])
+    def addKante(self, nameStartKnoten:Knoten, nameZielKnoten:Knoten, besucht:Knoten):
+        laenge = ((nameStartKnoten.x - nameZielKnoten.x) ** 2 + (nameStartKnoten.y - nameZielKnoten.y) ** 2) ** (0.5)
+        self.KantenMenge.append([nameStartKnoten.name , nameZielKnoten.name, laenge, besucht.besucht])
 
-
+    def getDistance(self, startKnoten, zielKnoten):
+        return ((startKnoten.x - zielKnoten.x) ** 2 + (startKnoten.y - zielKnoten.y) ** 2) ** 0.5
 
     def delKante(self, nameStartKnoten:int, nameZielKnoten:int):
         for kante in self.KantenMenge:
@@ -78,29 +101,116 @@ class Graph(object):
         nachbarn = []
         for kante in self.KantenMenge:
             if kante[0] == nameKnoten:
-                nachbarn.append(kante[1])
+                nachbarn.append(self.getKnoten(kante[1]))
+            elif kante[1] == nameKnoten:
+                nachbarn.append(self.getKnoten(kante[0]))
         return nachbarn
 
-    
-g = Graph()
-
-def getVerbindung():
-    for knoten in g.getAlleKnoten():
-        print(knoten.name, ":", g.getAlleNachbarn(knoten.name))
-
-def randomGraph(anzahlKnoten:int):
+def randomGraph(g:Graph, anzahlKnoten:int, besucht:bool):
     for i in range(anzahlKnoten):
-        g.addKnoten(i, "u")
+        xKoordinate = int(random.randint(0,100))
+        yKoordinate = int(random.randint(0,100))
+        g.addKnoten(i, xKoordinate, yKoordinate, besucht)
+
     for knoten in g.KnotenMenge:
         for i in range(len(g.KnotenMenge)):
             if i+1 == anzahlKnoten:
                 break
-            elif knoten.name == i+1:
+            elif knoten.name >= i+1:
                 continue
             else:
-                y = random.randint(0,11)
-                g.addKante(knoten.name, i+1, y)
+                g.addKante(g.KnotenMenge[knoten.name], g.KnotenMenge[i+1], g.KnotenMenge[knoten.besucht])
 
-    print(g.KantenMenge)
-    print(g.KnotenMenge)
-randomGraph(10)
+def nearestNeighbour(g:Graph, startKnotenName:int):
+    global besuchteKnoten 
+    startKnoten = g.getKnoten(startKnotenName)
+    startKnoten.besucht = True
+    besuchteKnoten = [startKnoten]
+    fremdeKnoten = []
+    for knoten in g.getAlleKnoten():
+        if not knoten.name == startKnoten.name:
+            fremdeKnoten.append(knoten)
+            
+    erster = besuchteKnoten[0]
+      
+    for i in range(len(fremdeKnoten)-1):
+        kuerzesterWeg = 2**0.5*100
+        ausgangsKnoten = besuchteKnoten[0]
+        nachbarn = g.getAlleNachbarn(ausgangsKnoten.name)
+        for nachbar in nachbarn:     
+            distance = g.getDistance(ausgangsKnoten, nachbar)
+            
+            if kuerzesterWeg > distance and nachbar.besucht == False:
+                kuerzesterWeg = distance
+                naehsterNachbar = nachbar
+
+        naehsterNachbar.besucht = True
+        besuchteKnoten.append(naehsterNachbar)   
+                    
+    besuchteKnoten.append(erster)
+    for knoten in besuchteKnoten:
+        print("besuchteKnoten: ",knoten.name)
+
+#Visualisierung
+def visualisierung(startKnoten):
+    yWerte = []
+    xWerte = []
+    xAbstand = []
+    yAbstand = []
+    for knoten in besuchteKnoten:
+        if startKnoten == knoten.name:
+            plt.text(knoten.x +1, knoten.y +1, str(knoten.name)+ " startknoten", color="black")
+        plt.text(knoten.x +1, knoten.y +1,knoten.name, color="black")
+        yWerte.append(knoten.y)
+        xWerte.append(knoten.x)
+    
+
+    print("y: ",yWerte)
+    print("x: ",xWerte)
+
+    plt.scatter(xWerte, yWerte,color='green', s=200)
+    
+    #abstände
+    for knoten in besuchteKnoten:
+        current = knoten
+        for i in range(len(besuchteKnoten)):
+            if i + 1 < len(besuchteKnoten) and i - 1 >= 0:
+                next = besuchteKnoten[i]
+                xLen = next.x - current.x
+                yLen = next.y - current.y
+                xAbstand.append(xLen)
+                yAbstand.append(yLen)
+                current = next
+
+        #letzten Abstand hinzufügen
+        xLen = knoten.x - current.x
+        yLen = knoten.y - current.y
+        xAbstand.append(xLen)
+        yAbstand.append(yLen)
+        break
+
+    #arrows
+    xWerte.pop()
+    yWerte.pop()
+    for a,b,c,d in it.zip_longest(xWerte, yWerte, xAbstand, yAbstand):
+        plt.arrow(a,b,c,d, width=.2, facecolor = "red", edgecolor="black")
+    
+    plt.title("Nearest Neighbour")
+    plt.xlabel("X-Werte")
+    plt.ylabel("Y-Werte")
+    plt.show()
+
+#Initialisierung des Graphen
+g = Graph()
+randomGraph(g, 5, False)
+
+startKnoten = 2
+#Berechnung des kürzesten Weges
+nearestNeighbour(g, startKnoten)
+
+#Visualisiere Graph
+visualisierung(startKnoten)
+
+
+
+
